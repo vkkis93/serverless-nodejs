c003ba062fb9683b50abec1385fb5e3eb51cdbbd
Viktor <vkkis1993@gmail.com>
2017-02-12 14:12:02 +0200

    git remove cached files

--- createUser.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const mongo = require('./mongo')
-module.exports.main = (event, context, callback) =>  {
-     console.log(process.env);
-    console.log(context)
-};
--- updateUser.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports.main = (event, context, callback) =>  {
-
-};
